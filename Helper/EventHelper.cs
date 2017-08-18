5267d38e21aaafc94a34cc2a30385bfd19acdf00
shrikantmane <Shrikant.Mane>
2017-08-18 15:24:09 +0530

    Added event api for multiple TO email address

--- Helper/EventHelper.cs
+++ Helper/EventHelper.cs
@@ -181,7 +181,36 @@ public async Task SendMultipleEmailAsync(
                 bodyBuilder.HtmlBody = mailOptions.htmlMessage;
             }
 
-            m.Body = bodyBuilder.ToMessageBody();
+           //create ics file for event
+                System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                string DateFormat = "yyyyMMddTHHmmssZ";
+                string now = DateTime.Now.ToUniversalTime().ToString(DateFormat);
+                sb.AppendLine("BEGIN:VCALENDAR");
+                sb.AppendLine("PRODID:-//Compnay Inc//Product Application//EN");
+                sb.AppendLine("VERSION:2.0");
+                sb.AppendLine("METHOD:PUBLISH");
+
+                sb.AppendLine("BEGIN:VEVENT");
+                sb.AppendLine("DTSTART:" + eventOptions.startTime.ToUniversalTime().ToString(DateFormat));
+                sb.AppendLine("DTEND:" + eventOptions.endTime.ToUniversalTime().ToString(DateFormat));
+                sb.AppendLine("DTSTAMP:" + now);
+                sb.AppendLine("UID:" + Guid.NewGuid());
+                sb.AppendLine("CREATED:" + now);
+                sb.AppendLine("X-ALT-DESC;FMTTYPE=text/html:" + eventOptions.eventDescription);
+                sb.AppendLine("LAST-MODIFIED:" + now);
+                sb.AppendLine("LOCATION:" + eventOptions.location);
+                sb.AppendLine("SEQUENCE:0");
+                sb.AppendLine("STATUS:CONFIRMED");
+                sb.AppendLine("SUMMARY:" + eventOptions.eventName);
+                sb.AppendLine("TRANSP:OPAQUE");
+                sb.AppendLine("END:VEVENT");
+
+                sb.AppendLine("END:VCALENDAR");
+
+                var calendarBytes = System.Text.Encoding.UTF8.GetBytes(sb.ToString());
+                System.IO.MemoryStream ms = new System.IO.MemoryStream(calendarBytes);
+                bodyBuilder.Attachments.Add("event.ics",ms);
+                m.Body = bodyBuilder.ToMessageBody();
 
             using (var client = new SmtpClient())
             {
